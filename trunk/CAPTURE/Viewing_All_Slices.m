a1b757be34a33406f8cdc0a7d8867bfb05e8312c
cdfbdex <cdfbdex@d32d564d-dd55-5bf2-e62d-b99ad55102b9>
2013-04-26 14:52:48 +0000

    Addin LIBS to capture NIFTI Files and some files to read and view slices.

---
+++ trunk/CAPTURE/Viewing_All_Slices.m
@@ -0,0 +1,7 @@
+clc;
+addpath('LIBS_CAPTURE');        % In this directory load_nii exists;
+Nifti = load_nii(...
+        '../DATA/NIFTI/DATA_1/rsource_mri.hdr');
+Nifti                   %Printing Nifti Object properties.
+
+view_nii(Nifti);
