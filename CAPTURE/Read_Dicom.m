257138ad2fce7c9e231b958c91d08e9d0657de92
cdfbdex <cdfbdex@d32d564d-dd55-5bf2-e62d-b99ad55102b9>
2013-04-26 14:52:48 +0000

    Addin LIBS to capture NIFTI Files and some files to read and view slices.

--- CAPTURE/Read_Dicom.m
+++ CAPTURE/Read_Dicom.m
@@ -1,6 +1,7 @@
-Y = dicomread('../DATA/DICOM_SAMPLES/SOUS-702/IM-0001-0001.dcm');
-imtool(Y,'DisplayRange',[])
-min(min(Y))
-max(max(Y))
+clc;
+Y = dicomread('../DATA/DICOM_SAMPLES/SOUS-702/IM-0001-0022.dcm');
+imshow(Y)
+imcontrast;
+size(Y);
 
 
